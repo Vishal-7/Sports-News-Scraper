0aab247271c0ca3baa204bd3ce3ade3e1546dc7b
Vishal <32330754+Vishal-7@users.noreply.github.com>
2020-09-17 11:03:00 -0500

    Python Code to Scrape the news

--- webBoxer.py
+++ webBoxer.py
@@ -3,31 +3,43 @@ from bs4 import BeautifulSoup # importing to scrape the data from the webpage
 import ssl # importing to check the certificate of the website
 import csv # importing to report the output to a comma seperated file
 from datetime import date
+link_dict = {}
 
-test_dict = dict()
-tag_dict = dict()
-link = "https://www.google.com/search?safe=active&biw=820&bih=695&tbs=qdr%3Ad&tbm=nws&sxsrf=ALeKk01hNXjAYnGEs02fdHhZcSPXKYsjoA%3A1600039726396&ei=LqteX47jF4WgsQWE052wCw&q=modi&oq=modi&gs_l=psy-ab.3..0i433k1l2j0l8.145750.146098.0.147011.4.4.0.0.0.0.164.556.0j4.4.0....0...1c.1.64.psy-ab..0.4.553....0.xnA42ZM6Lhk"
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE # verifying the certificate
-req = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-html = urllib.request.urlopen(req).read()
-soup = BeautifulSoup(html, 'lxml')
+html = urllib.request.urlopen("https://www.indiatoday.in/sports", context=ctx).read() # open the webpage and get the html content of the webpage requested as a request object
+soup = BeautifulSoup(html, 'lxml') # parsing the object using beautiful soup to traverse through the html document
 today = date.today()
-# print(soup.prettify())
-for i in soup.find_all('div',class_="ZINbbc xpd O9g5cc uUPGi"):
-    link = i.find('a',href=True)['href']
-    test_link = link.split("/url?q=")[1]
-    final_link = test_link.split("&sa=")[0]
-    heading =  (i.find('div',class_="BNeawe vvjwJb AP7Wnd").text)
-    passage = i.find('div',class_="BNeawe s3v9rd AP7Wnd").text
+csv_file = open('news_portal_'+str(today)+'.csv','w') # creates a csv file with the write mode to add lines to the file
+csv_writer = csv.writer(csv_file,lineterminator='\n') # used to remove the newline in the csv file
+csv_writer.writerow(['Article Name','Article Content','Date Posted','Tags','Author'])
+top_stories = soup.find('div',class_="special-top-news")
+# print(top_stories.prettify())
+for i in top_stories.find_all('li',class_=""):
+    title = i.attrs['title']
+    link = "https://www.indiatoday.in/"+str(i.a.attrs['href'])
+    link_dict[title] = link
+for n_headlines,n_link in link_dict.items():
+    content = []
+    tags = []
     
-    print(heading)
-    print(final_link)
-    print(passage)
-    print("----------------------------------------------")
-# csv_file = open('news_portal_'+str(today)+'.csv','w') # creates a csv file with the write mode to add lines to the file
-# csv_writer = csv.writer(csv_file,lineterminator='\n') # used to remove the newline in the csv file
-# csv_writer.writerow(['Article Name','Article Content','Date Posted','Tags','Author'])
-# print(soup.prettify())
-
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE # verifying the certificate
+    html = urllib.request.urlopen(n_link, context=ctx).read() # open the webpage and get the html content of the webpage requested as a request object
+    soup = BeautifulSoup(html, 'lxml')
+    try:
+        for i in soup.find_all('span', class_="tag-follow"):
+            tags.append(i.a.text)
+        final_tags = ",".join(tags)
+        date_posted = soup.find('dt', class_="pubdata").text
+        posted_by = soup.find('span', class_="posted-name").text
+        content_test = soup.find('div',{"itemprop" : "articleBody"}).find_all('p')
+        for sample in content_test:
+            content.append(sample.text)
+        final_content = "".join(content)
+    except:
+        continue
+    csv_writer.writerow([n_headlines,final_content,date_posted,final_tags,posted_by])
+csv_file.close()
